ee99f2b5b2fff6f789d5dd1a2735a46cb2da0032
Sanidhya Vijay <103128503+Sanidhyavj@users.noreply.github.com>
2024-02-08 00:15:56 +0530

    Update topo.cpp

--- GRAPH/TOPO SORT/topo.cpp
+++ GRAPH/TOPO SORT/topo.cpp
@@ -1,1 +1,35 @@
 
+	void dfs(int node,stack<int> &st,vector<int> adj[],int vis[])
+	{
+	    vis[node] = 1;
+	    
+	    for(auto it:adj[node])
+	    {
+	        if(!vis[it])
+	        {
+	            dfs(it,st,adj,vis);
+	        }
+	    }
+	    st.push(node);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    int vis[V] = {0};
+	    stack<int> st;
+	    
+	    for(int i=0;i<V;i++)
+	    {
+	        if(!vis[i])
+	        {
+	            dfs(i,st,adj,vis);
+	        }
+	    }
+	    vector<int> ans;
+	    while(!st.empty())
+	    {
+	        ans.push_back(st.top());
+	        st.pop();
+	    }
+	    return ans;
+	}
