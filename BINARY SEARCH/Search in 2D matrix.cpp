830800adeb61e709fbebc0db59df876dcb52bfa3
Sanidhya Vijay <103128503+Sanidhyavj@users.noreply.github.com>
2024-05-18 00:48:41 +0530

    Create Search in 2D matrix.cpp

---
+++ BINARY SEARCH/Search in 2D matrix.cpp
@@ -0,0 +1,26 @@
+//Brute force O(n*m)
+// 2nd approach find row in which target is bounded then apply Binary Search O(n*logm)
+// 3rd approach -
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int l = 0;
+        int r = m*n -1;
+
+        while(l<=r)
+        {
+            int mid = (l+r)/2;
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col]==target) return true;
+
+            if(matrix[row][col]<target)
+            l = mid+1;
+            else
+            r = mid-1;
+        }
+        return false;
+    }
+};
