a0b1612dccfd9257b94a1e87c31f84763ae84aba
Sanidhya Vijay <103128503+Sanidhyavj@users.noreply.github.com>
2024-06-03 15:00:57 +0530

    Create Unique Paths.cpp

---
+++ DP/2D DP/Unique Paths.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    private :
+    int func(int i,int j,vector<vector<int>> &dp)
+    {
+        if(i==0 && j==0)
+        return 1;
+        if(i<0 || j<0)
+        return 0;
+
+        if(dp[i][j] != -1)
+        return dp[i][j];
+
+        int up = func(i-1,j,dp);
+        int left = func(i,j-1,dp);
+
+        return dp[i][j] = up+left;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0) continue;
+
+                int up = 0;
+                int left = 0;
+                if(i>0)  up = dp[i-1][j];
+                if(j>0)  left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
